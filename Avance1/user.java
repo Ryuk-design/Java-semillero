1c60800e51157eb2863eab6816a1bf78bf88fb4e
César Santiago Malaver <96667561+SantyxM21@users.noreply.github.com>
2026-02-23 16:48:49 +0000

    CarpetasCreadas

---
+++ Avance1/user.java
@@ -0,0 +1,14 @@
+public class user{
+    private int id;
+    private String email;
+    private String name;
+    private String password;
+    private String linkedIn_user;
+    private String github_user;
+    private int level;
+    private String role;
+    private int current_semester;
+    private String profile_photo;
+
+    
+    }
